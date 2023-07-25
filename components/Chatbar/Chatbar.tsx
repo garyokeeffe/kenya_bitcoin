2d5a2c3aa8ce46b34ef2efb727b80ef41eb2f51d
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-24 22:45:51 -0700

    more translations

--- components/Chatbar/Chatbar.tsx
+++ components/Chatbar/Chatbar.tsx
@@ -222,7 +222,7 @@ export const Chatbar = () => {
       <Sidebar<Conversation>
         side={'left'}
         isOpen={showChatbar}
-        addItemButtonTitle={t('Chat mpya')}
+        addItemButtonTitle={t('Chat Mpya')}
         itemComponent={<Conversations conversations={filteredConversations} />}
         folderComponent={<ChatFolders searchTerm={searchTerm} />}
         items={filteredConversations}
