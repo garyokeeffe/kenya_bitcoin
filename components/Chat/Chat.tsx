586fef25641d5779331126edf88c008d19d9dacd
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-24 22:36:44 -0700

    rebrand to Master Guantai

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,7 +352,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to Master Guantai
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
@@ -405,7 +405,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'Master Guantai'
                     )}
                   </div>
 
--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,7 +386,7 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Master Guantai
         </a>
         .{' '}
         {t(
